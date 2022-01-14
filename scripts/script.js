484dc4bb2ab8821c8e8ccd98255a93a4df3ad91d
Adrian Piwin <adrian7048@gmail.com>
2022-01-14 12:10:54 -0500

    Initial Commit
    - Added website layout
    - TODO: projects & about

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function openNav(){
+    document.getElementById("mySidenav").style.transform = "translate(-250px)";
+}
+
+function closeNav(){
+    document.getElementById("mySidenav").style.transform = "translate(0px)";
+}
